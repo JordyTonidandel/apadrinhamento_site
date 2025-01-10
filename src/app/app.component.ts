6e69c359ecdfda340c4c6eb1274af5534aaef358
Jordy Tonidandel <jtonidandel@osas.com.br>
2025-01-10 12:08:17 -0300

    Ajustes icone logo e responsividade para todos os tipos de mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'encanto_apadrinhamento';
+  
   hideHeaderFooter: boolean = false;
   private routesToHideHeaderFooter = ['/login'];
 
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { faCoffee, faMusic } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
+
+  faCoffee = faCoffee;
+  faMusic = faMusic;
+  user = localStorage.getItem('user');
 
   logout() {
     this.authService.logout();
@@ -20,4 +26,11 @@ export class HeaderComponent {
     return !!localStorage.getItem('token');
   }
 
+  navigateOnClick(): void {
+    if (this.isLoggedIn()) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.router.navigate(['/home']);
+    }
+  }
 }
